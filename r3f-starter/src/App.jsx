13f7f749b450f61c46f4869bee7a0a1cea65ed76
KenTra222 <Kentraviousc@gmail.com>
2023-02-01 06:39:26 +0000

    working on loading textures

--- r3f-starter/src/App.jsx
+++ r3f-starter/src/App.jsx
@@ -1,35 +1,30 @@
 import { useState } from 'react'
 import ReactDOM from 'react-dom'
 import './App.css'
-import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
+import { Clone, OrbitControls, useGLTF, useTexture } from '@react-three/drei'
 import { useControls } from 'leva'
  
  
 
-import { Canvas } from '@react-three/fiber'
 
-function App() {
-  const model = useGLTF('./lunch.glb')
-  console.log(model)
+
+export default function App() {
+  const { nodes} = useGLTF("/lunch.glb");
+  const modelTexture = useTexture('/lunch.jpg')
+   console.log(modelTexture)
+
+
   return (
     <div id="canvas-container">
-      
       <Canvas>
-      <OrbitControls makeDefault />
-        {/* objects */}
-        <ambientLight intensity={0.9} />
-      <directionalLight color="red" position={[0, 0, ]} />
-      <mesh color='blue'>
-        <boxGeometry  />
-        <meshStandardMaterial />
-      </mesh> 
-
-      {/* lights */}
-      </Canvas>
       
+      <color args={ [ '#030202' ] } attach="background" />      
+      <OrbitControls makeDefault />      
+      <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+           
+      </Canvas>
     </div>
   )
 }
+useGLTF.preload("/lunch.glb");
 
-
-export default App

81b490e6702619029b8569c781f7698a6824e2ee
KenTra222 <Kentraviousc@gmail.com>
2023-02-16 04:28:21 +0000

    added hamburger icon

--- kentravious_colson/src/main.jsx
+++ kentravious_colson/src/main.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.scss'
+import { IconContext } from 'react-icons';
+import { TiThMenuOutline } from "react-icons/ti";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className='App-Wrapper'>
       <div className='header'>
         <div className='header-left'>
-          logo
+
+          KC
         </div>
         <div className='header-center'>
-          3d component
+          3d Component
         </div>
+
+   
         <div className='header-right'>
-          hamburger menu
+            <button className='hamburger_menu'>
+              <TiThMenuOutline />
+            </button>
         </div>
+    
       </div>      
     </div>
   </React.StrictMode>,

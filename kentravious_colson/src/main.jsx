37f79b35b636f07cfb01dfd77074469009e20e51
KenTra222 <Kentraviousc@gmail.com>
2023-02-16 03:42:51 +0000

    added sass and react icons

--- kentravious_colson/src/main.jsx
+++ kentravious_colson/src/main.jsx
@@ -1,62 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Experience from './Experience'
-import './index.css'
-import { Canvas } from '@react-three/fiber';
-import Form from './Form';
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Canvas className='Experience'
-     flat
-     camera={ {
-         fov: 45,
-         near: 0.1,
-         far: 50,
-         position: [ 1, 2, 6 ]
-     } }>
-      <Experience />
-    </Canvas>
-
-    <div className='Website'>
-
-      <section className='work'>
-        <h2>My Work</h2>
-        <div className='portfolio'> 
-          <ul className='categories'>
-            <li>
-              <h4>Real World</h4>
-              <ul className='projects'>
-                <li className='project1'></li>
-                <li className='project1'></li>
-                <li className='project1'></li>
-              </ul>
-            </li>
-            <li> 
-              <h4>For fun</h4>
-              <ul className='projects'>
-                  <li className='project1'></li>
-                  <li className='project1'></li>
-                  <li className='project1'></li>
-              </ul>
-            </li>
-            <li> 
-              <h4>Artwork</h4>
-              <ul className='projects'>
-                  <li className='project1'></li>
-                  <li className='project1'></li>
-                  <li className='project1'></li>
-              </ul>
-            </li>
-          </ul>
+    <div className='App-Wrapper'>
+      <div className='header'>
+        <div className='header-left'>
+          logo
         </div>
-
-      </section>
-
-      <section className='contact'>
-        <h2>Contact Me</h2>
-         <Form/>
-      </section>
+        <div className='header-center'>
+          3d component
+        </div>
+        <div className='header-right'>
+          hamburger menu
+        </div>
+      </div>      
     </div>
   </React.StrictMode>,
 )

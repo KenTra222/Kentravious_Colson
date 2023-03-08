ca6122de1ea52428c2626f07672e9aa8749006df
KenTra222 <Kentraviousc@gmail.com>
2023-03-08 06:52:37 +0000

    form sends data

--- kentravious_colson/src/Components/Form/Form.jsx
+++ kentravious_colson/src/Components/Form/Form.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './form.scss'
 import { useForm } from 'react-hook-form';
+import emailjs from '@emailjs/browser';
 
 export default function Form() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = data => console.log('hi');
   //console.log(errors);
   
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_8gizhqi', 'template_b97bbqi', form.current, 'Mfd4LCYeJOiwD1XzY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
-    
-            
     <div className='contact_contents'>
     
       <div className='info-wrapper'>
@@ -25,18 +37,18 @@ export default function Form() {
           </div>
         </div>
 
-    <form className='form' onSubmit={handleSubmit(onSubmit)}>
+    <form ref={form} className='form' onSubmit={sendEmail}>
       <label>
       Name
-      <input className='input' type="text" placeholder="Name" {...register("Name", {})} />
+      <input className='input' type="text" placeholder="Name" {...register("user_name", {})} />
       </label>
       <label>
       Email
-      <input className='input' type="email" placeholder="Email" {...register("Email", {})} />
+      <input className='input' type="email" placeholder="Email" {...register("user_email", {})} />
       </label>
       <label>
       Describe your project
-      <textarea {...register("Short Message", {})} placeholder=" leave a message"/>
+      <textarea {...register("message", {})} placeholder=" leave a message"/>
       </label>
 
       <input className='submit' type="submit" />

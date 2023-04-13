c0f420092aa7ebf75581a5643a2ce7e1a91de9e1
KenTra222 <Kentraviousc@gmail.com>
2023-04-13 04:44:59 +0000

    added github link

--- kentravious_colson/src/Components/Footer/Footer.jsx
+++ kentravious_colson/src/Components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './footer.scss'
 import {FaLinkedin} from 'react-icons/fa'
+import {BsGithub} from 'react-icons/bs'
 
 export const Footer = () => {
   return (
@@ -10,6 +11,7 @@ export const Footer = () => {
       <p>&copy; {new Date().getFullYear()} Kentravious Colson</p>
       <ul className="social-icons">
         <li><a href="https://www.linkedin.com/in/kentravious-colson-33802a216" target="_blank" rel="noopener noreferrer"><FaLinkedin/></a></li>
+        <li><a href="https://github.com/KenTra222" target="_blank" rel="noopener noreferrer"><BsGithub/></a></li>
        
       </ul>
     </div>

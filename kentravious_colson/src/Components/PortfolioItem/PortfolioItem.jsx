3541c455e15d57ffdc010f45db40332dcfa78519
KenTra222 <Kentraviousc@gmail.com>
2023-04-14 23:01:22 +0000

    fixing portfolio section

--- kentravious_colson/src/Components/PortfolioItem/PortfolioItem.jsx
+++ kentravious_colson/src/Components/PortfolioItem/PortfolioItem.jsx
@@ -7,27 +7,20 @@ export const PortfolioItem = ({ title, imgUrl, stack, link, product }) => {
   return (
     
       <div className='portfolioItem_wrapper'>
-         
-
           <img
               src={imgUrl}
               alt='portfolio'
               className='portfolio_img'/>
 
-              <div className='item_details'>
-             
-                            <h3>{title}</h3>
+              <div className='item_details'>             
+                <h3>{title}</h3>
                              
-                 
-              
-                  <p className='stack'> STACK: 
-                      {stack.map(item =>(
-                          <span> {item} </span>
-                          ))}
-                  </p>
-                
-              </div>
-                         
+                <p className='stack'> STACK: 
+                    {stack.map(item =>(
+                        <span> {item} </span>
+                        ))}
+                </p>               
+              </div>                    
       </div>
      
   )

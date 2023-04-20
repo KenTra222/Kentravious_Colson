c35e9c1d87b36bc752a928a99effa066c54eb097
KenTra222 <Kentraviousc@gmail.com>
2023-04-20 04:27:30 +0000

    added 3d component

--- kentravious_colson/src/Components/About/About.jsx
+++ kentravious_colson/src/Components/About/About.jsx
@@ -30,7 +30,7 @@ const About = () => {
         "I'm a self-taught <span className='dev_text'>Frontend Developer</span> with a passion for creating exceptional digital experiences using design systems and programming languages. My journey in tech started in 2020 while I was working at a car shop and discovered my love for learning how to talk to computers through watching YouTube videos.
         <br/>
         <br/>
-        Since then, I've been on a mission to stay up-to-date with the latest software and tools, completing the Three.js course to further improve my knowledge and skills. I also have a strong interest in 3D animation and love using Blender to create unique and engaging designs.
+        Since then, I've been on a mission to stay up-to-date with the latest software and tools, completing the Three.js course to further improve my knowledge and skills. I also have a strong interest in 3D animation and love using Blender to create unique and engaging designs. "If you're on a desktop hover your mouse here to see cool spacehip".
         <br/>
         <br/>
         Currently, I work remotely with a select group of freelance clients, but I'm always open to new opportunities and challenges. If you have a project you'd like to collaborate on, please feel free to reach out to me. You can check out some of my work on my portfolio."

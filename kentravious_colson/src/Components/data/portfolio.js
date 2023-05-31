c06c886591c3137ce413e3b239ae978f5f07fba0
KenTra222 <Kentraviousc@gmail.com>
2023-05-31 20:42:48 +0000

    added new sections to site

--- kentravious_colson/src/Components/data/portfolio.js
+++ kentravious_colson/src/Components/data/portfolio.js
@@ -2,35 +2,44 @@ export default [
     {
         id: 1,
         title: 'claudthacreator',
-        imgUrl: '/assets/ctc.jpg',
+        imgUrl: '/assets/projects/ctc.jpg',
         stack: ['WIX'],
         link: 'https://www.claudthacreator.com/',
         product: 'Website',
         description: "The aim of this project is to create a personal portfolio website for a tattoo artist in Atlanta who is LGBT friendly. The website will showcase the artist's portfolio, highlight their expertise in tattooing, and provide an easy-to-use platform for clients to book appointments."
     },
-
     {
         id: 2,
+        title: 'Copious Staffing Solution',
+        imgUrl: '/assets/projects/copiousStaffing.png',
+        stack: ['WIX, javascript'],
+        link: 'https://joy076.wixsite.com/copious-staffing-sol',
+        product: 'website',
+        description: "I created an employment agency from scratch using the Wix platform for design, while also implementing JavaScript code to interact with the Wix API and maintain and filter the database."
+    },
+
+    {
+          id: 3,
         title: 'Portfolio Site',
-        imgUrl: '/assets/portfolio2.png ',
+        imgUrl: '/assets/projects/portfolio2.png ',
         stack: ['React |', 'SCSS  |', 'Vite |', 'R3F '],
         link: '#',
         product: '3D Artwork',
         description: "My personal portfolio, created to showcase the passion, creativity, and expertise that I bring to every project.  I hope this portfolio demonstrates my dedication to quality and my ability to solve problems in innovative and effective ways.  Thank you for visiting."
     },
     {
-        id: 3,
+              id: 4,
         title: 'Cadet landing Page',
-        imgUrl: '/assets/cadet.jpg',
+        imgUrl: '/assets/projects/cadet.jpg',
         stack: ['Figma'],
         link: 'https://www.figma.com/file/2st9LypPJgSbpUBByWZwXk/cadet-landing-page?node-id=0%3A1&t=6XL8TqASQxVCO4Fq-1',
         product: 'Web Design',
         description: " The landing page for the space camp is a visually stunning and modern design that is meant to capture the imagination and excitement of potential participants. The page features a clean, minimalist layout with striking imagery that showcases the wonder of space and the thrill of exploration."
     },
     {
-        id: 4,
+        id: 5,
         title: 'gigibite',
-        imgUrl: '/assets/gigibite.jpg',
+        imgUrl: '/assets/projects/gigibite.jpg',
         stack: ['Figma'],
         link: 'https://www.figma.com/proto/90wwOJrU5nhAWZXyzLUadN/Untitled?node-id=2-2&starting-point-node-id=2%3A2',
         product: 'Web Design',

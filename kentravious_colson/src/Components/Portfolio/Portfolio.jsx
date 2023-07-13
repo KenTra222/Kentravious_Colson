a941bccac74679b01d23410b082356cbccdcf1ec
KenTra222 <Kentraviousc@gmail.com>
2023-07-13 02:09:33 +0000

    added project slider

--- kentravious_colson/src/Components/Portfolio/Portfolio.jsx
+++ kentravious_colson/src/Components/Portfolio/Portfolio.jsx
@@ -2,22 +2,54 @@ import React, {useRef} from 'react'
 import portfolio from "../data/portfolio.js"
 import { PortfolioItem } from '../PortfolioItem/PortfolioItem';
 import './portfolio.scss'
+import {Canvas} from '@react-three/fiber'
+import { Center, OrbitControls, PresentationControls } from '@react-three/drei';
+import {motion} from 'framer-motion-3d'
+import { atom, useAtom } from 'jotai'
+
+
+export const currentProjectAtom = atom(Math.floor(portfolio.length / 2))
 
 
 
 export const Portfolio = () => {
- 
+
+  const [currentProject] = useAtom(currentProjectAtom)
+
+
  
   return (
     <section id='projects' className='section' >
-        <h2 className='section_header'>03.<span>My Recent Projects!</span></h2>           
+        <h2 className='section_header'><span>My Recent Projects!</span></h2>           
               
     <div className='portfolio_wrapper'>
-          <div className='portfolio'>
+        <Canvas>
+              <Center>
+              {portfolio.map((project, id) => (
+                <motion.group 
+                key={project.id} 
+                position={[id * 2.5, 0, 2]}
+                animate={{
+                  x: 0 + (id - currentProject) * 2.5,
+                  y: currentProject === id ? 0 : -0.1,
+                  z: currentProject === id ? -2 : -3,
+                  
+                }}>
+                <PortfolioItem
+                  imgUrl={project.imgUrl}
+                  title={project.title.toUpperCase()}
+                  link={project.link}
+                  description={project.description}
+                  highlighted={id===currentProject}
+                  />           
+            </motion.group>
+                ))}
           
+                </Center>
             
-          </div>
+        </Canvas>
     </div>
+    
           </section>
             
   )
--- kentravious_colson/src/Components/data/portfolio.js
+++ kentravious_colson/src/Components/data/portfolio.js
@@ -2,47 +2,47 @@ export default [
     {
         id: 1,
         title: 'claudthacreator',
-        imgUrl: '/assets/projects/ctc.jpg',
+        imgUrl: 'projects/ctc.jpg',
         stack: ['WIX'],
         link: 'https://www.claudthacreator.com/',
         product: 'Website',
-        description: "The aim of this project is to create a personal portfolio website for a tattoo artist in Atlanta who is LGBT friendly. The website will showcase the artist's portfolio, highlight their expertise in tattooing, and provide an easy-to-use platform for clients to book appointments."
+        description: "A personal portfolio website for a tattoo artist in Atlanta who is LGBTQIA+ friendly."
     },
     {
         id: 2,
         title: 'Copious Staffing Solution',
-        imgUrl: '/assets/projects/copiousStaffing.png',
+        imgUrl: 'projects/copiousStaffing.png',
         stack: ['WIX, javascript'],
         link: 'https://joy076.wixsite.com/copious-staffing-sol',
         product: 'website',
-        description: "I created an employment agency from scratch using the Wix platform for design, while also implementing JavaScript code to interact with the Wix API and maintain and filter the database."
+        description: "I created an employment agency from scratch using the Wix platform for design."
     },
 
     {
           id: 3,
         title: 'Portfolio Site',
-        imgUrl: '/assets/projects/portfolio2.png ',
+        imgUrl: 'projects/portfolio2.png ',
         stack: ['React |', 'SCSS  |', 'Vite |', 'R3F '],
         link: '#',
         product: '3D Artwork',
-        description: "My personal portfolio, created to showcase the passion, creativity, and expertise that I bring to every project.  I hope this portfolio demonstrates my dedication to quality and my ability to solve problems in innovative and effective ways.  Thank you for visiting."
+        description: "My personal portfolio, created to showcase the passion, creativity, and expertise that I bring to every project."
     },
     {
-              id: 4,
+        id: 4,
         title: 'Cadet landing Page',
-        imgUrl: '/assets/projects/cadet.jpg',
+        imgUrl: 'projects/cadet.jpg',
         stack: ['Figma'],
         link: 'https://www.figma.com/file/2st9LypPJgSbpUBByWZwXk/cadet-landing-page?node-id=0%3A1&t=6XL8TqASQxVCO4Fq-1',
         product: 'Web Design',
-        description: " The landing page for the space camp is a visually stunning and modern design that is meant to capture the imagination and excitement of potential participants. The page features a clean, minimalist layout with striking imagery that showcases the wonder of space and the thrill of exploration."
+        description: " The landing page for the space camp is a visually stunning and modern design that is meant to capture the imagination and excitement of potential participants."
     },
     {
         id: 5,
         title: 'gigibite',
-        imgUrl: '/assets/projects/gigibite.jpg',
+        imgUrl: 'projects/gigibite.jpg',
         stack: ['Figma'],
         link: 'https://www.figma.com/proto/90wwOJrU5nhAWZXyzLUadN/Untitled?node-id=2-2&starting-point-node-id=2%3A2',
         product: 'Web Design',
-        description: "A Figma design file of a tech e-commerce shop. This design to captivate visitors and showcase cutting-edge products in the most visually appealing way possible. This file is incorporated with various cool animations and transitions that not only make the website stand out but also make it a delight to use."
+        description: "A Figma design file of a tech e-commerce shop. This design to captivate visitors and showcase cutting-edge products in the most visually appealing way possible. ."
     } 
 ]

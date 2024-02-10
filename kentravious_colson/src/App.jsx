da7d707314c57ed1abbf59f3b83f4058737db00b
KenTra222 <Kentraviousc@gmail.com>
2024-02-10 20:40:43 +0000

    added basic text structure

--- kentravious_colson/src/App.jsx
+++ kentravious_colson/src/App.jsx
@@ -40,15 +40,142 @@ function App() {
       </header>
 
       <main>
+        <section className="HERO_WRAPPER">
          <div>
-          <p> skill overview</p>
+          <p> Creative Developer/Designer</p>
          </div>
          <div>
-          <p> skill overview</p>
+          <p>Kentravious Colson</p>
          </div>
          <div>
-          <p> skill overview</p>
+          <p> Strong passion for creating user strategies, content, designs and assets to make your businesses / project easier for you and your clients</p>
          </div>
+         <div>
+          <a> CONTACT</a>
+         </div>
+         <div>
+          <p>Visual</p>
+         </div>
+        </section>
+
+        <section className="LANGUAGE_WRAPPER">
+          <div>
+            <ul>
+              <li>html</li>
+              <li>css</li>
+              <li>javascript</li>
+              <li>React</li>
+              <li>Figma</li>
+              <li>Blender</li>
+            </ul>
+          </div>
+        </section>
+
+        <section className="SKILLS_WRAPPER">
+          <div>
+            <p>image</p>
+            <p>Title</p>
+            <p>description</p>
+          </div>
+
+          <div>
+            <p>image</p>
+            <p>Title</p>
+            <p>description</p>
+          </div>
+
+          <div>
+            <p>image</p>
+            <p>Title</p>
+            <p>description</p>
+          </div>
+        </section>
+        
+        <section className="PROJECT_WRAPPER">
+          <div>      
+            <div>
+             <p>Image</p>
+            </div>
+            <p>Title</p>
+            <p>category</p>
+          </div>
+
+          <div>      
+            <div>
+             <p>Image</p>
+            </div>
+            <p>Title</p>
+            <p>category</p>
+          </div>
+
+          <div>      
+            <div>
+             <p>Image</p>
+            </div>
+            <p>Title</p>
+            <p>category</p>
+          </div>
+
+          <div>      
+            <div>
+             <p>Image</p>
+            </div>
+            <p>Title</p>
+            <p>category</p>
+          </div>
+
+          <div>      
+            <div>
+             <p>Image</p>
+            </div>
+            <p>Title</p>
+            <p>category</p>
+          </div>
+
+          <div>      
+            <div>
+             <p>Image</p>
+            </div>
+            <p>Title</p>
+            <p>category</p>
+          </div>
+        </section>
+
+        <section className="ARTWORK_WRAPPER">
+            <div>
+              <div><p>image</p></div>
+              <p>title</p>
+            </div>
+        </section>
+        
+        <section className="TESTIMONIAL_WRAPPER">
+            <div>
+              <p>testimony</p>
+
+              <div>
+                <p> image </p>
+                <div>
+                  <p>rating</p>
+                  <p>name</p>
+                  <p>site</p>
+                </div>
+              </div>
+            </div>
+        </section>
+
+        <section className="CONTACT_WRAPPER">
+          <div>
+            <h2>Let's Work Together </h2>
+            <p>contact text</p>
+            <nav> 
+              <p> social media bar</p>
+            </nav>
+          </div>
+
+          <form>
+
+          </form>
+        </section>
       </main>
       <footer>
           footer

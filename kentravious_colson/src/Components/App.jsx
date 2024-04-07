13099e0d21361506481b8488624bc84a4bb8331b
KenTra222 <Kentraviousc@gmail.com>
2024-04-07 04:16:09 +0000

    updated homepage

--- kentravious_colson/src/Components/App.jsx
+++ kentravious_colson/src/Components/App.jsx
@@ -1,45 +1,52 @@
-
-import { Canvas } from '@react-three/fiber'
 import React, {useRef, useState, useEffect } from 'react'
     
 const App = () => {  
   
-  
   return (
+
     <div className='Container'>
-        <header className=''> 
+      {/* header */}
+        <header className='header'> 
           <div className='title_wrapper'>
-            <div>ball</div>
-            <div><p><span><a>Kentravious Colson</a></span></p></div>
-          </div>
-
-          <div>
-            <ul>
-              <li><div><a><p>resume</p></a></div></li>
-              <li><div><a><p>projects</p></a></div></li>
-              <li><div><a><p>about</p></a></div></li>
-              <li><div><a><p>services</p></a></div></li>
-              <li><div><a><p>contact</p></a></div></li>
-
+            <div className='title_ball'></div>
+            <div className='title'><p><a>Kentravious Colson</a></p></div>
+          </div>    
+        {/* navbar */}
+          <nav className='nav_bar'>
+            <ul className='menu'>
+                <li className='menu_item'><a>resume </a></li>
+                <li className='menu_item'><a>projects</a></li>
+                <li className='menu_item'><a>about</a></li>
+                <li className='menu_item'><a>services</a></li>
+                <li className='menu_item'><a>contact</a></li>
             </ul>
-          </div>
+          </nav>
         </header>
 
+      {/* main */}
         <main>
           <div className='homepage_container'>
             <div className='image_div_Container'></div>
-            <div className='rich_text_containter'><h1>Hey There!</h1></div>
+
+            <div className='hero_content_container'>
+
+            <div className='rich_text_containter'><h1>Hey <br/>there!</h1></div>
             <div className='rich_text_containter'><p>Summary</p></div>
             <div className='rich_text_containter'><p>As a Creative Frontend Web Dev with 4 years of freelancing experience, I use my knowledge and skills in UX/UI Design, Content Management, and Coding to ensure that my clients leave with products that are not only satisfying to look at but also reliable and useful.</p></div>
+            
+            <div className='links_container'>
 
-            <div className='circle_btn'><p><a>Resume</a></p></div>
-            <div className='circle_btn'><p><a>Projects</a></p></div>
-            <div className='circle_btn'><p><a>Contact</a></p></div>
+              <div className='circle_btn'><p><a>Resume</a></p></div>
+              <div className='circle_btn'><p><a>Projects</a></p></div>
+              <div className='circle_btn'><p><a>Contact</a></p></div>
+            </div>
+            </div>
 
 
           </div>
         </main>
 
+        {/* footer */}
         <footer>
           <div>
             <p>

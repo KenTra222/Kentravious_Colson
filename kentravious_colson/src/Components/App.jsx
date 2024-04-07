62df0543f7a96f69c6b6427691e657763ecb2e1d
KenTra222 <Kentraviousc@gmail.com>
2024-04-07 09:26:08 +0000

    added resume and project mark up

--- kentravious_colson/src/Components/App.jsx
+++ kentravious_colson/src/Components/App.jsx
@@ -25,25 +25,75 @@ const App = () => {
 
       {/* main */}
         <main>
-          <div className='homepage_container'>
+          {/* <div className='homepage_container'>
             <div className='image_div_Container'></div>
 
             <div className='hero_content_container'>
 
             <div className='rich_text_containter'><h1>Hey <br/>there!</h1></div>
-            <div className='rich_text_containter'><p>Summary</p></div>
-            <div className='rich_text_containter'><p>As a Creative Frontend Web Dev with 4 years of freelancing experience, I use my knowledge and skills in UX/UI Design, Content Management, and Coding to ensure that my clients leave with products that are not only satisfying to look at but also reliable and useful.</p></div>
+
+            <div className='text_container'>
+            <div ><p className='rich_text_subtitle'>Summary</p></div>
+            <div ><p className='rich_text_summary'>As a Creative Frontend Web Dev with 4 years of freelancing experience, I use my knowledge and skills in UX/UI Design, Content Management, and Coding to ensure that my clients leave with products that are not only satisfying to look at but also reliable and useful.</p></div>
+            </div>
             
             <div className='links_container'>
 
-              <div className='circle_btn'><p><a>Resume</a></p></div>
-              <div className='circle_btn'><p><a>Projects</a></p></div>
-              <div className='circle_btn'><p><a>Contact</a></p></div>
+              <div id='blue_circle' className='circle_btn'><p  className='cirle_red'><a>Resume</a></p></div>
+              <div id='yellow_circle' className='circle_btn'><p  className='cirle_blue'><a>Projects</a></p></div>
+              <div id='red_circle' className='circle_btn'><p  className='cirle_yellow'><a>Contact</a></p></div>
             </div>
             </div>
+          </div> */}
 
+        {/* resume */}
+        {/* <div> <h2>My resume</h2></div>
+        <div><h3> Work Experience</h3></div>
+        <div>
+          <div>year</div><div>title</div><div>what I did</div>
+          <div>year</div><div>title</div><div>what I did</div>
+          <div>year</div><div>title</div><div>what I did</div>
+        </div>
 
-          </div>
+        <div><h3> Education and Certs</h3></div>
+        <div>
+          <div>year</div><div>title</div><div>what I did</div>
+          <div>year</div><div>title</div><div>what I did</div>
+          <div>year</div><div>title</div><div>what I did</div>
+        </div>
+
+        <div><h3> Skills and Expertise</h3></div>
+        <div>
+          <div>year</div><div>title</div><div>what I did</div>
+          <div>year</div><div>title</div><div>what I did</div>
+          <div>year</div><div>title</div><div>what I did</div>
+        </div> */}
+
+        {/* Projects */}
+        <div>
+          <h2>Projects</h2>
+        </div>
+
+        <div className='project_wrapper'>
+          <div> <h3>title</h3> <p>description</p></div>
+          <div> <div> image link</div></div>
+        </div>
+
+        <div className='project_wrapper'>
+          <div> <h3>title</h3> <p>description</p></div>
+          <div> <div> image link</div></div>
+        </div>
+
+        <div className='project_wrapper'>
+          <div> <h3>title</h3> <p>description</p></div>
+          <div> <div> image link</div></div>
+        </div>
+
+        <div className='project_wrapper'>
+          <div> <h3>title</h3> <p>description</p></div>
+          <div> <div> image link</div></div>
+        </div>
+
         </main>
 
         {/* footer */}

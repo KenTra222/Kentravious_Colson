b4689a0b5655cced60a3e86716e7939531b32900
KenTra222 <Kentraviousc@gmail.com>
2024-04-16 01:24:33 +0000

    embedded google form

--- kentravious_colson/src/Components/Form/Form.jsx
+++ kentravious_colson/src/Components/Form/Form.jsx
@@ -1,32 +1,14 @@
 import './form.scss'
-import React from 'react'
-import { useForm } from 'react-hook-form';
+ 
 
 const Form = () => {
 
-     
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
-  console.log(errors);
   return (
       
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>
-        Fullname
-      </label>
-      <input type="text" placeholder="Full Name" {...register} />
-
-      <label>
-        Email
-      </label>
-      <input type="email" placeholder="Email" {...register("Email", {})} />
-
-      <label>
-        Message
-      </label>
-      <textarea placeholder='Message' {...register("Message", {})} />
-
-      <input className='submit_btn' type="submit" />
+    <form >
+      <a className='submit_btn'href='https://docs.google.com/forms/d/e/1FAIpQLSdIxJ9Ssb6U96lmtHhyT3TY1HK6IsZ0HcRrraAoH5fsHtpKvw/viewform?usp=sf_link' >
+          Contact Now
+      </a>
     </form>
  
     
